c94a903ad9e49586c2d22db306d9eb9937a00248
don2vito <43207483+don2vito@users.noreply.github.com>
2021-05-15 13:52:58 +0800

    更新代码

--- 自动收发邮件/send_mail_example.py
+++ 自动收发邮件/send_mail_example.py
@@ -1,4 +1,4 @@
-﻿# import keyring
+# import keyring
 # keyring.set_password('yagmail','邮箱地址','SMTP码')
 
 '''
@@ -22,4 +22,27 @@ from imbox import Imbox
 
 pwd = keyring.get_password('yagmail','邮箱地址')
 with Imbox('邮箱的 imap 网址','邮箱地址',pwd) as imbox:
-    pass
+    all_inbox_messages = imbox.messages()
+    for uid, message in all_inbox_messages:
+        print(message.subject)
+        print(message.body['plain'])
+
+'''
+message.sent_from 发件人
+message.sent_to 收件人
+message.subject 主题
+message.date 时间
+message.body[‘plain’] 文本格式内容
+message.body[‘html’] html格式内容
+message.attchments 附件
+'''
+
+# 未读邮件：imbox.message(unread=True)
+# 红旗邮件: imbox.messages(flagged=True)
+# 某发件人的邮件: imbox.messages(sent_from='发件人邮箱')
+# 某收件人的邮件: imbox.messages(sent_to='收件人邮箱')
+# date__lt 某天前：imbox.messages(date__lt = datetime.date(年,月,日))
+# date__gt 某天后：imbox.messages(date__gt = datetime.date(年,月,日))
+# date__on 指定某一天：imbox.messages(date__on = datetime.date(年,月,日))
+# 标记已读： imbox.mark_seen(uid)
+# 删除邮件： imbox.delete(uid)
