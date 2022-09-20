f2cc4a8d2ad7c95a9ba79dc71aeba4f4bd76749a
don2vito <43207483+don2vito@users.noreply.github.com>
2022-09-20 11:10:59 +0800

    更新代码

--- 多种方式美化输出表格和进度条/美化输出.py
+++ 多种方式美化输出表格和进度条/美化输出.py
@@ -8,6 +8,10 @@ from rich.progress import track
 from rich import print
 import time
 from rich.pretty import pprint
+# from pprint import pprint
+from rich.markdown import Markdown
+from rich.syntax import Syntax
+import json
 
 
 # 构建虚拟的 dataframe
@@ -46,7 +50,8 @@ print(f'使用 prettytable 美化输出 dataframe：\n{pt}')
 
 # 使用 rich_dataframe 美化输出 dataframe
 tb = prettify(df,row_limit=10,delay_time=1)
-print(f'使用 rich_dataframe 美化输出 dataframe：\n{tb}')
+print('使用 rich_dataframe 美化输出 dataframe：\n')
+print(tb)
 
 
 # 使用 rich 美化输出 dataframe
@@ -68,3 +73,55 @@ console.print(table)
 # 使用 rich 美化输出 进度条
 for i in track(range(5), description='Processing...'):
     time.sleep(1)
+    
+    
+# 使用 rich 美化输出 markdown
+console = Console()
+with open('如何打造爆品.md',encoding='UTF-8') as readme:
+    markdown = Markdown(readme.read())
+
+print('使用 rich 美化输出 markdown：\n')
+console.print(markdown)
+
+
+# 使用 rich 实现语法高亮显示
+my_code = '''
+def iter_first_last(values: Iterable[T]) -&gt; Iterable[Tuple[bool, bool, T]]:
+    """Iterate and generate a tuple with a flag for first and last value."""
+    iter_values = iter(values)
+    try:
+        previous_value = next(iter_values)
+    except StopIteration:
+        return
+    first = True
+    for value in iter_values:
+        yield first, False, previous_value
+        first = False
+        previous_value = value
+    yield first, True, previous_value
+'''
+syntax = Syntax(my_code, 'python', theme='monokai', line_numbers=True)
+console = Console()
+
+print('使用 rich 实现语法高亮显示：\n')
+console.print(syntax)
+
+
+# 使用 rich 美化输出 JSON
+data = {
+    "foo": [
+        3.1427,
+        (
+            "Paul Atreides",
+            "Vladimir Harkonnen",
+            "Thufir Hawat",
+        ),
+    ],
+    "atomic": (False, True, None),
+} 
+
+data = json.dumps(data, sort_keys=True, indent=2)
+data = data.encode('utf-8').decode('unicode_escape')
+
+print('使用 rich 美化输出 JSON：\n')
+print(data)
